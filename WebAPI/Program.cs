fe54acebc38c30be3de55df944c04b95cd24b439
abrahaamv <velizabrahaam@gmail.com>
2024-12-08 02:20:43 -0600

    Update Program.cs

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("MyPharmacyDatabase");
+builder.Services.AddDbContext<ProductsContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
