a53d5bedfa75c7cb4030569aaeb3e2167ea6150d
abrahaamv <velizabrahaam@gmail.com>
2024-12-08 01:48:26 -0600

    Update Product.cs

--- WebAPI/Entities/Products/Product.cs
+++ WebAPI/Entities/Products/Product.cs
@@ -7,16 +7,22 @@ public class Product
     public required string Name { get; set; }
     public required string Description { get; set; }
     public required string Slug { get; set; }
+    //BRAND
     public int BrandId { get; set; }
     public required Brand Brand { get; set; }
+    //CATEGORY
     public int CategoryId { get; set; }
     public required Category Category { get; set; }
+    //SUBCATEGORY
     public int? SubCategoryId { get; set; }
     public Subcategory? SubCategory { get; set; }
+    //PRODUCT SPECIFICATIONS
     public List<Specification>? Specifications { get; set; }
     public required List<string> ImageUrls { get; set; }
+    //PRICE
     public decimal ListPrice { get; set; }
     public decimal SellPrice { get; set; }
+    //AVAILABLE QUANTITY
     public int Stock { get; set; }
     public bool IsInStock => Stock > 0;
 }
