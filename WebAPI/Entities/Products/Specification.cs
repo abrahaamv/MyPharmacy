697df71c69c85a9ccde3d6812c0570841515be28
abrahaamv <velizabrahaam@gmail.com>
2024-12-08 14:23:17 -0600

    Update Specification.cs

--- WebAPI/Entities/Products/Specification.cs
+++ WebAPI/Entities/Products/Specification.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace WebAPI.Entities.Products;
 
+[NotMapped]
 public class Specification
 {
     public required string Name { get; set; }
