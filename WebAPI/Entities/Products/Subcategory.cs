632a1c46dc9cd4cdde009a13aa82ba3444e1d96c
abrahaamv <velizabrahaam@gmail.com>
2024-12-09 16:41:08 -0600

    Update Subcategory.cs

--- WebAPI/Entities/Products/Subcategory.cs
+++ WebAPI/Entities/Products/Subcategory.cs
@@ -5,6 +5,4 @@ public class Subcategory
     public int Id { get; set; }
     public required string Name { get; set; }
     public int CategoryId { get; set; }
-    public ICollection<Product>? Products { get; set; }
-    
 }
