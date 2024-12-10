a0f9c1d11e4c890827209c159c86582def1aeb26
abrahaamv <velizabrahaam@gmail.com>
2024-12-09 21:16:04 -0600

    Create ProductSummaryDto.cs

---
+++ WebAPI/Dtos/Products/ProductSummaryDto.cs
@@ -0,0 +1,14 @@
+namespace WebAPI.Dtos.Products;
+
+public record ProductSummaryDto(
+    int Id,
+    string Ean,
+    string Name,
+    string Slug,
+    string Brand, 
+    string Category,
+    string SubCategory,
+    decimal ListPrice,
+    decimal SellingPrice, 
+    bool IsInStock,
+    List<string> ImageUrls);
