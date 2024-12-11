d6f86e1952e2e14dfe3bff51d9da007e1689ba62
abrahaamv <velizabrahaam@gmail.com>
2024-12-10 19:32:10 -0600

    Create UpdateProductDto.cs

---
+++ WebAPI/Dtos/Products/UpdateProductDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using WebAPI.Entities.Products;
+namespace WebAPI.Dtos.Products;
+
+public record UpdateProductDto(
+    [Required] string Ean,
+    [Required] string Name,
+    [Required]string Description,
+    string Slug,
+    [Required] int BrandId,
+    int CategoryId,
+    int SubCategoryId,
+    [Required] decimal ListPrice,
+    [Required] decimal SellingPrice, 
+    [Required] int Stock,
+    [Required] List<string> ImageUrls,
+    List<Specification> Specifications);
