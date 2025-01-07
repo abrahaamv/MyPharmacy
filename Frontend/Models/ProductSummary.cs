ae2cd8ca68899304449dc5ecfb97a62e97f9c955
abrahaamv <velizabrahaam@gmail.com>
2025-01-06 20:56:56 -0600

    Update ProductSummary.cs

--- Frontend/Models/ProductSummary.cs
+++ Frontend/Models/ProductSummary.cs
@@ -1,17 +1,37 @@
 namespace Frontend.Models;
+using System.Text.Json.Serialization;
 
 public class ProductSummary
 {
     public int Id { get; set; }
-    public required string Ean { get; set; }
-    public required string Name { get; set; }
-    public required string Slug { get; set; }
-    public  required string Brand { get; set; }
-    public required string Category { get; set; }
-    public string? SubCategory { get; set; }
-    public decimal ListPrice { get; set; }
-    public decimal SellingPrice { get; set; }
-    public bool IsInStock { get; set; }
-    public required List<string> ImageUrls { get; set; }
-}
 
+    [JsonPropertyName("ean")]
+    public string Ean { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("slug")]
+    public string Slug { get; set; }
+
+    [JsonPropertyName("brand")]
+    public string Brand { get; set; }
+
+    [JsonPropertyName("category")]
+    public string Category { get; set; }
+
+    [JsonPropertyName("subCategory")]
+    public string SubCategory { get; set; }
+
+    [JsonPropertyName("listPrice")]
+    public decimal ListPrice { get; set; }
+
+    [JsonPropertyName("sellingPrice")]
+    public decimal SellingPrice { get; set; }
+
+    [JsonPropertyName("isInStock")]
+    public bool IsInStock { get; set; }
+
+    [JsonPropertyName("imageUrls")]
+    public List<string> ImageUrls { get; set; }
+}
