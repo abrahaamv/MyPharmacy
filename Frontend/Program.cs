b2e1ffc7bdb95571cca1886dd37c03d58c1c3b1e
abrahaamv <velizabrahaam@gmail.com>
2025-01-13 13:28:17 -0600

    Update Program.cs

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -31,6 +31,7 @@ if (!app.Environment.IsDevelopment())
 app.UseAntiforgery();
 
 app.MapStaticAssets();
-app.MapRazorComponents<App>();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
